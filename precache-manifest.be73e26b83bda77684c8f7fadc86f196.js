d2cf7f1f92a93a201d9e144eee6949c171370e13
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 15:16:40 +0100

    Updates

---
+++ precache-manifest.be73e26b83bda77684c8f7fadc86f196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be32f84cc00985b9d16643c404670de7",
+    "url": "/FirstReactApp/index.html"
+  },
+  {
+    "revision": "160c86e2e743f8677b86",
+    "url": "/FirstReactApp/static/css/main.9f5292e6.chunk.css"
+  },
+  {
+    "revision": "ec4b0bbe371e0c109be8",
+    "url": "/FirstReactApp/static/js/2.952a9e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstReactApp/static/js/2.952a9e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160c86e2e743f8677b86",
+    "url": "/FirstReactApp/static/js/main.5cf7ee10.chunk.js"
+  },
+  {
+    "revision": "bb1c733e256194b20743",
+    "url": "/FirstReactApp/static/js/runtime-main.ada868e4.js"
+  }
+]);
